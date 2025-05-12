e3fc3f012e45cc9e66959f0c4352d31256efc1e6
vasantha Perugu <perugu.vasantha@gmail.com>
2025-05-12 03:56:56 +0000

    added few nire stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 import sys
+import string
+
+#Define stopwords
+
+stopwords = set(['the', 'and', 'a', 'for', 'I', 'go', 'be', 'railings', 'or', 'not'])
+translator = string.maketrans('', '')
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    # make lower case
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
-
-    # output tuples (word, 1) in tab-delimited format
+    line = line.translate(translator, string.punctuation)
+   
+	
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+	    print '%s\t%s' % (word, "1")
